4672095818bfa041c79bb5e166881a8599d1a77c
Alexandr Larionov <alarionov93@yandex.ru>
2021-10-03 12:25:37 +0500

    created kp drawing and affine warp works better now

--- cp_finder.py
+++ cp_finder.py
@@ -2,8 +2,8 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img1 = cv.imread('/Volumes/Data/PhotoDevNew/Export2021/Export_13_07_21/0732-0733/IMG-0732.jpg')
-img2 = cv.imread('/Volumes/Data/PhotoDevNew/Export2021/Export_13_07_21/0732-0733/IMG-0733.jpg')
+img1 = cv.imread('/Users/sanya/Work/py_stacking/tst_imgs/IMG-1874_s.jpg')
+img2 = cv.imread('/Users/sanya/Work/py_stacking/tst_imgs/IMG-1873_s.jpg')
 
 orb = cv.ORB_create()
 
@@ -31,13 +31,25 @@ matches = bf.knnMatch(des1, des2, k=2)
 # cutted_matches = matches [ : len(matches)-1]
 good = []
 for m,n in matches:
-	if m.distance <  0.7*n.distance:
-		# print(np.array(kp0[m.trainIdx].pt) - np.array(kp1[m.queryIdx].pt))
+	if m.distance <  0.85*n.distance: # 0.85 - ok; TODO: other - test it
 		good.append(m)
 
-# src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-# dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+		# print(np.array(kp0[m.trainIdx].pt) - np.array(kp1[m.queryIdx].pt))
+src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ])
+dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ])
 
+h, stat = cv.findHomography(src_pts, dst_pts)
+# cv.drawKeypoints(img1, kp1, img1)
+# cv.drawKeypoints(img2, kp2, img2)
+for y,x in src_pts:
+	cv.circle(img1,(int(y),int(x)), 4, (0,0,255), -1)
+
+for y,x in dst_pts:
+	cv.circle(img2,(int(y),int(x)), 4, (0,255,0), -1)
+
+img_out = cv.warpPerspective(img1, h, (img2.shape[1]+3000, img2.shape[0]+2000))
+cv.imwrite('/Users/sanya/Work/py_stacking/tst_imgs/IMG-1874_warped.jpg', img_out)
+cv.imwrite('/Users/sanya/Work/py_stacking/tst_imgs/IMG-1873_kp.jpg', img2)
 # M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
 
 # matchesMask = mask.ravel().tolist()
@@ -46,7 +58,7 @@ for m,n in matches:
 # pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
 # dst = cv.getPerspectiveTransform(img2,M)
 
-img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-plt.imshow(img3),plt.show()
+# img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+# plt.imshow(img3),plt.show()
 
 
